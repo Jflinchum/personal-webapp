fcbba49b19e79244cc0ba9e51cfc1c5b2cc07ea5
Jflinchum <jonflinchum@gmail.com>
2025-02-23 17:38:52 -0500

    list socials on personal details

--- components/PersonalDetailsCard/PersonalDetailsCard.tsx
+++ components/PersonalDetailsCard/PersonalDetailsCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import './PersonalDetailsCard.css';
+import Socials from '../Socials/Socials'
 
 
 const PersonalDetailsCard = () => {
@@ -12,7 +13,7 @@ const PersonalDetailsCard = () => {
         <Image width='200' height='200' src='/personal-webapp/Jonathan_Flinchum.jpg' alt='Picture of Jonathan Flinchum'/>
 
         <h1 className='personal-name'>Jonathan Flinchum</h1>
-        <a href="mailto:jonflinchum@gmail.com">jonflinchum@gmail.com</a>
+        <a href="mailto:jonflinchum@gmail.com" aria-label="Email jonflinchum@gmail.com">jonflinchum@gmail.com</a>
       </div>
 
       <hr/>
@@ -27,6 +28,7 @@ const PersonalDetailsCard = () => {
       <Link href="/resume" className='personal-primary-button'>Learn More</Link>
 
       <hr/>
+      <Socials />
     </div>
   );
 }
